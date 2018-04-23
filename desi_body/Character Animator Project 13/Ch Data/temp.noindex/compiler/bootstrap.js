293334211e7a5d31b56c9a5a7644d2a90e165da0
DevOpHBLICT <AppDevelopment@hblict.nhs.uk>
2018-04-23 14:18:25 +0100

    'sdfgsdf'

---
+++ desi_body/Character Animator Project 13/Ch Data/temp.noindex/compiler/bootstrap.js
@@ -0,0 +1,3 @@
+var __bootStrapPath = "C:\\desi_body\\Character Animator Project 13\\Ch Data\\temp.noindex\\compiler\\bootstrap.js";
+var __compiledScene = "C:\\desi_body\\Character Animator Project 13\\Ch Data\\temp.noindex\\compiler\\scene.json";
+runScript("C:\\Program Files\\Adobe\\Adobe Character Animator CC 2018\\Support Files\\Runtime\\animal\\scripts\\js\\lib\\engine.js")
