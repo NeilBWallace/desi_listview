17ed2cbee12d4a70501ea6fdfc197cdc092d5df2
DevOpHBLICT <AppDevelopment@hblict.nhs.uk>
2018-04-25 11:19:48 +0100

    'sfs'

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,57 @@
 ﻿
 
 using UnityEngine;
+using UnityEngine.AI;
 
+
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
-
+	
+	Camera cam;
+	public LayerMask movementMask;
 	public Animation anim;
+
+	PlayerMotor motor;
+
 
 	void Start(){
 		anim = GetComponent<Animation>();
-
+		cam =Camera.main;
+		motor = GetComponent<PlayerMotor> ();
 	}
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
 
-		var walkSpeed = Input.GetAxis ("Vertical");
-		Debug.Log (walkSpeed);
-		if (Mathf.Abs(walkSpeed) > 0.1)  {
-			anim.Play("stand");
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit)) {
+				Debug.Log ("we hit" + hit.collider.name + " " + hit.point);	}
+		motor.MoveToPoint (hit.point);
+		
+		//	var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		//	var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		//	var walkSpeed = Input.GetAxis ("Vertical");
+		//	Debug.Log ("rotation" + x);
+		//	Debug.Log (walkSpeed);
+
+	//		if ((Mathf.Abs(x) > 0.05) || (Mathf.Abs(walkSpeed) > 0.05)) {
+
+				
+	//		}
+	//		else {
+	//			anim.Stop("stand");
+	//		}
+
 		}
-		else {
-			anim.Stop("stand");
-		}
+	
+
+	//	transform.Rotate(0, x, 0);
+	//	transform.Translate(0, 0, z);
+
+	
 	}
 }
